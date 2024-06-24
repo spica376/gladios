a25df0aa5636b11cecfcea83a9d811f19cc40a9e
spica <kimyh090402@gmail.com>
2024-06-24 18:06:51 +0900

    fix second

--- rmtts.py
+++ rmtts.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c519b41e1a6820eaac435feff0c2546f09358e2f4ec8e7f9b88eea6466256341
-size 8310
+oid sha256:55bf342ceacf1332508a6765d31dda20d211884c6187729ddadeb0129d2cffb3
+size 8330
