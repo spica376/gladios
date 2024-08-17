2eeef15ccb52ab7b29495624828a401d1f7014d7
spica <kimyh090402@gmail.com>
2024-08-17 13:49:58 +0900

    'testing_discordbotadded'

---
+++ discorder.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:786a284d75f3265609744d47ed6fdc608403b4e6dba299a357143781accf8033
+size 5853
--- rmtts.py
+++ rmtts.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3b27a2208202b3fb9b6604891af01143443e7ccb7de3889577026367fc6f315
-size 9336
+oid sha256:99b7802acd60000c79cf35b0d8e90d0b3ebb3a48213e7e20be334f80be821ca1
+size 9511
